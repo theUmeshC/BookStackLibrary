d54c5d295553ec4cfc3ee84f9a7f67031d785d9d
theUmeshC <umesh.c@celestialsys.com>
2022-12-09 15:46:52 +0530

    added CRUD endpoints from backend

--- Backend/Controllers/libraryController.js
+++ Backend/Controllers/libraryController.js
@@ -9,7 +9,6 @@ export const getList = (req, res) => {
 
 // controller to handle the adding of a new book to database library table
 export const postBook = (req, res) => {
-  console.log(req.body);
   const title = req.body.title;
   const author = req.body.author;
   const publishDate = req.body.publishDate;
@@ -25,4 +24,66 @@ export const postBook = (req, res) => {
       return res.json(data);
     })
     .catch((err) => console.log(err));
+};
+
+// controller to get the book wrt id from the database table
+export const editBook = (req, res) => {
+  const bookId = req.params.bookId;
+  Library.findAll({ where: { id: bookId } })
+    .then((book) => {
+      return res.json(book);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// controller to handle updating the book details wrt to the id
+export const postEditBook = (req, res) => {
+  const bookId = req.params.bookId;
+  const updatedTitle = req.body.title;
+  const updatedAuthor = req.body.author;
+  const updatedPublishDate = req.body.publishDate;
+  const updatedImageUrl = req.body.imageUrl;
+  Library.findAll({ where: { id: bookId } })
+    .then(([book]) => {
+      book.title = updatedTitle;
+      book.author = updatedAuthor;
+      book.imageUrl = updatedImageUrl;
+      book.publishDate = updatedPublishDate;
+      return book.save();
+    })
+    .then(() => {
+      console.log("updated product");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// controller to delete the book details wrt to the id
+export const deleteBook = (req, res) => {
+  const bookId = req.params.bookId;
+  Library.findAll({ where: { id: bookId } })
+    .then(([book]) => {
+      return book.destroy();
+    })
+    .then(() => {
+      console.log("deleted book");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// controller to fetch a book wrt to title from library table from database
+export const fetchBookByTitle = (req, res) => {
+  const bookTitle = req.params.bookTitle;
+  Library.findAll({ where: { title: bookTitle } })
+    .then((book) => {
+      return res.json(book);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- Backend/Routes/libraryRoutes.js
+++ Backend/Routes/libraryRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 
-import { getList, postBook } from '../Controllers/libraryController.js';
+import { editBook, fetchBookByTitle, getList, postBook, postEditBook } from '../Controllers/libraryController.js';
 
+//setting up the express router
 export const router = express.Router();
 
 // router to get book list from database
@@ -9,3 +10,12 @@ router.get('/bookList', getList);
 
 // router to add book to database
 router.post('/addBookToList', postBook);
+
+// router to get book from database table wrt to id
+router.get('/editBook:bookId', editBook);
+
+// router to updating book after editing to database table wrt to id
+router.post('/postEditBook:bookId', postEditBook);
+
+// router to fetch book from db table wrt to title
+router.get('/fetchByTitle:bookTitle', fetchBookByTitle)
